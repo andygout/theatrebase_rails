e016f1a4abd293fc81109fb54677b178f7cdd457
andygout <andygout@hotmail.co.uk>
2015-09-22 15:53:34 +0100

    Amend: Set production_params to private in productions_controller.rb

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -36,8 +36,10 @@ class ProductionsController < ApplicationController
     redirect_to productions_path
   end
 
-  def production_params
-    params.require(:production).permit(:title)
-  end
+  private
+
+    def production_params
+      params.require(:production).permit(:title)
+    end
 
 end
