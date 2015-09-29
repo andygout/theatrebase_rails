2e20ddc8d644ce00b2ab70449cd0e6b22a4470e5
andygout <andygout@hotmail.co.uk>
2015-09-29 15:08:48 +0100

    Tidy: Test syntax

--- spec/features/users_login_spec.rb
+++ spec/features/users_login_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-feature 'user log in' do
+feature 'User log in' do
   context 'valid details' do
     let!(:user) { create :user }
     scenario 'should redirect to user page with success message', js: true do
       visit login_path
       expect(page).to have_selector :link, 'Log in'
+      expect(page).not_to have_selector :link, 'Profile'
       expect(page).not_to have_selector :link, 'Log out'
       fill_in 'session_email',    with: "#{user.email}"
       fill_in 'session_password', with: "#{user.password}"
       click_button 'Log in'
-      expect(page).to have_css('div.alert-success')
-      expect(page).not_to have_css('div.alert-error')
+      expect(page).to have_css 'div.alert-success'
+      expect(page).not_to have_css 'div.alert-error'
+      expect(page).to have_selector :link, 'Profile'
       expect(page).to have_selector :link, 'Log out'
       expect(page).not_to have_selector :link, 'Log in'
       expect(current_path).to eq "/users/1"
@@ -25,13 +27,14 @@ feature 'user log in' do
       fill_in 'session_email',    with: "#{invalid_user[:email]}"
       fill_in 'session_password', with: "#{invalid_user[:password]}"
       click_button 'Log in'
-      expect(page).to have_css('div.alert-error')
-      expect(page).not_to have_css('div.alert-success')
+      expect(page).to have_css 'div.alert-error'
+      expect(page).not_to have_css 'div.alert-success'
       expect(current_path).to eq login_path
       expect(page).to have_selector :link, 'Log in'
+      expect(page).not_to have_selector :link, 'Profile'
       expect(page).not_to have_selector :link, 'Log out'
       visit root_path
-      expect(page).not_to have_css('div.alert-error')
+      expect(page).not_to have_css 'div.alert-error'
     end
   end
 end
--- spec/features/users_signup_spec.rb
+++ spec/features/users_signup_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'user sign-up' do
+feature 'User sign-up' do
   context 'valid details' do
     let(:user) { attributes_for :user }
     scenario 'should redirect to user page with success message', js: true do
@@ -10,9 +10,9 @@ feature 'user sign-up' do
       fill_in 'user_password',              with: "#{user[:password]}"
       fill_in 'user_password_confirmation', with: "#{user[:password_confirmation]}"
       expect { click_button 'Create User' }.to change { User.count }.by 1
-      expect(page).to have_css('div.alert-success')
-      expect(page).not_to have_css('div.alert-error')
-      expect(page).not_to have_css('li.field_with_errors')
+      expect(page).to have_css 'div.alert-success'
+      expect(page).not_to have_css 'div.alert-error'
+      expect(page).not_to have_css 'li.field_with_errors'
       expect(page).to have_selector :link, 'Profile'
       expect(page).to have_selector :link, 'Log out'
       expect(page).not_to have_selector :link, 'Log in'
@@ -29,9 +29,9 @@ feature 'user sign-up' do
       fill_in 'user_password',              with: "#{invalid_user[:password]}"
       fill_in 'user_password_confirmation', with: "#{invalid_user[:password_confirmation]}"
       expect { click_button 'Create User' }.to change { User.count }.by 0
-      expect(page).to have_css('div.alert-error')
-      expect(page).to have_css('li.field_with_errors')
-      expect(page).not_to have_css('div.alert-success')
+      expect(page).to have_css 'div.alert-error'
+      expect(page).to have_css 'li.field_with_errors'
+      expect(page).not_to have_css 'div.alert-success'
       expect(page).to have_selector :link, 'Log in'
       expect(page).not_to have_selector :link, 'Profile'
       expect(page).not_to have_selector :link, 'Log out'
