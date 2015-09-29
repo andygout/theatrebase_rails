42226392d740e528f1e854a2cef122ecfab63207
andygout <andygout@hotmail.co.uk>
2015-09-29 14:41:20 +0100

    Add: Login with valid details functionality & test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show show page
+      log_in user
+      flash[:success] = "Successfully logged in as #{user.name} (#{user.email})"
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email-password combination'
       render :new
@@ -14,6 +16,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    flash[:success] = 'Successfully logged out'
+    redirect_to root_path
   end
 
 end
--- spec/features/users_login_spec.rb
+++ spec/features/users_login_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 feature 'user log in' do
+  context 'valid details' do
+    let!(:user) { create :user }
+    scenario 'should redirect to user page with success message', js: true do
+      visit login_path
+      expect(page).to have_selector :link, 'Log in'
+      expect(page).not_to have_selector :link, 'Log out'
+      fill_in 'session_email',    with: "#{user.email}"
+      fill_in 'session_password', with: "#{user.password}"
+      click_button 'Log in'
+      expect(page).to have_css('div.alert-success')
+      expect(page).not_to have_css('div.alert-error')
+      expect(page).to have_selector :link, 'Log out'
+      expect(page).not_to have_selector :link, 'Log in'
+      expect(current_path).to eq "/users/1"
+    end
+  end
+
   context 'invalid details' do
     let(:invalid_user) { attributes_for :invalid_user }
     scenario 'should re-render form with error message', js: true do
@@ -11,6 +28,8 @@ feature 'user log in' do
       expect(page).to have_css('div.alert-error')
       expect(page).not_to have_css('div.alert-success')
       expect(current_path).to eq login_path
+      expect(page).to have_selector :link, 'Log in'
+      expect(page).not_to have_selector :link, 'Log out'
       visit root_path
       expect(page).not_to have_css('div.alert-error')
     end
