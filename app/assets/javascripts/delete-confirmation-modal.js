36cc6d4053ccb10c3aa4c512ed68c9e78118d815
andygout <andygout@hotmail.co.uk>
2015-11-07 12:37:42 +0000

    Add: Modal reacts to window resize

--- app/assets/javascripts/delete-confirmation-modal.js
+++ app/assets/javascripts/delete-confirmation-modal.js
@@ -1,9 +1,5 @@
 var timeMillisecs = 500;
-var timeSecs = millisecsToSecs(timeMillisecs);
-
-function millisecsToSecs(millisecs) {
-  return millisecs / 1000;
-}
+var timeSecs = timeMillisecs / 1000;
 
 $.rails.allowAction = function(link) {
   if (!link.attr('data-confirm')) {
@@ -23,48 +19,71 @@ $.rails.activateModal = function(link) {
 
   createOverlay();
 
-  var modalHTML = acquireModalHTML(link);
+  createModal(link);
 
-  $(modalHTML)
-    .hide()
-    .appendTo('body');
+  blurContainer(2);
 
-    showModal();
+  $(window).resize(function() {
+    centreModal();
+  });
 
-    $('.confirm').click(function() {
-      $.rails.confirmed(link);
-    });
+  $(document).mouseup(function (e)
+  {
+    var container = $('.modal');
+    if (!container.is(e.target) && container.has(e.target).length === 0)
+    {
+      removeEffect();
+    }
+  });
 
-    $('.cancel').click(function() {
-      removeModal();
-    });
+  $('.confirm').click(function() {
+    $.rails.confirmed(link);
+  });
+
+  $('.cancel').click(function() {
+    removeEffect();
+  });
 };
+
+function removeEffect() {
+  blurContainer(0);
+
+  $('.overlay, .modal')
+    .fadeOut(timeMillisecs, function() {
+      $(this).remove();
+      setBodyOverflowY('auto');
+    });
+}
 
 function setBodyOverflowY(value) {
   $('body').css('overflow-y', value);
 }
 
 function createOverlay() {
-  $('<div class="overlay"></div>')
+  $("<div class='overlay'></div>")
     .css('opacity', 0.5)
     .fadeIn(timeMillisecs)
     .appendTo('body');
 }
 
-function acquireModalHTML(link) {
+function createModal(link) {
   var model = link.attr('data-model');
-  return "<div class='modal'><h1 class='header-text'>Really delete this " + model + "?</h1><button class='button confirm'>OK</button><button class='button cancel'>Cancel</button></div>";
+  var modalHTML = "<div class='modal'><h1 class='header-text'>Really delete this " + model + "?</h1><button class='button confirm'>OK</button><button class='button cancel'>Cancel</button></div>";
+
+  $(modalHTML)
+    .hide()
+    .fadeIn(timeMillisecs)
+    .appendTo('body');
+
+  centreModal();
 }
 
-function showModal() {
+function centreModal() {
   $('.modal')
     .css({
       'top': acquireModalTop(),
       'left': acquireModalLeft()
-    })
-    .fadeIn(timeMillisecs);
-
-    blurContainer(2);
+    });
 }
 
 function acquireModalTop() {
@@ -84,12 +103,14 @@ function blurContainer(blurSize) {
       'mozFilter': filterVal,
       'oFilter': filterVal,
       'msFilter': filterVal
-    })
-    containerTransition('ease-out');
+    });
+
+  containerTransition('ease-out');
 }
 
 function containerTransition(effect) {
-  var transitionVal = 'all ' + timeSecs + 's ' + effect
+  var transitionVal = 'all ' + timeSecs + 's ' + effect;
+
   $('.container')
     .css({
       'transition': transitionVal,
@@ -97,24 +118,5 @@ function containerTransition(effect) {
       '-moz-transition': transitionVal,
       '-o-transition': transitionVal,
       '-ms-transition': transitionVal
-    })
-}
-
-$(document).mouseup(function (e)
-{
-  var container = $('.modal');
-  if (!container.is(e.target) && container.has(e.target).length === 0)
-  {
-    removeModal();
-  }
-});
-
-function removeModal() {
-  blurContainer(0);
-
-  $('.overlay, .modal')
-    .fadeOut(timeMillisecs, function() {
-      $(this).remove();
-      setBodyOverflowY('auto');
     });
 }
