c78e0995c43058c5e052609f044c8d99b7786e5b
andygout <andygout@hotmail.co.uk>
2015-12-05 13:15:57 +0000

    Add: Password reset functionality

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -18,4 +18,22 @@ describe UserMailer, type: :mailer do
     end
   end
 
+  describe 'password reset' do
+    let(:user) { create :user }
+    let(:mail) { UserMailer.password_reset(user) }
+
+    it 'renders the headers' do
+      user.reset_token = User.new_token
+      expect(mail.subject).to eq('Password reset')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'renders the body' do
+      user.reset_token = User.new_token
+      expect(mail.body.encoded).to match(user.name)
+      expect(mail.body.encoded).to match(user.reset_token)
+      expect(mail.body.encoded).to match(CGI::escape(user.email))
+    end
+  end
 end
