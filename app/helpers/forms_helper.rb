8587dc228be9b0e958c49e217a5714cbe79b8151
andygout <andygout@hotmail.co.uk>
2016-04-03 09:50:33 +0100

    Add: Creator/updater/assignor email address to edit page info

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -7,7 +7,7 @@ module FormsHelper
   end
 
   def created_updated_text at, by
-    at && by ? "#{datetime_format(at)} by #{by.name}" : 'TBC'
+    at && by ? "#{datetime_format(at)} by #{by.name} (#{by.email})" : 'TBC'
   end
 
   def get_created_updated_info var
@@ -18,7 +18,9 @@ module FormsHelper
   end
 
   def get_status_assigned_info status
-    assigned = status ? "#{datetime_format(status.created_at)} by #{status.assignor.name}" : 'TBC'
+    assigned = status ?
+      "#{datetime_format(status.created_at)} by #{status.assignor.name} (#{status.assignor.email})" :
+      'TBC'
     markup = bookend_div_tags(bookend_table_tags(compile_rows([['Assigned:', assigned]])), 'content-container')
     @status_assigned_info = markup.html_safe
   end
