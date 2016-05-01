8c39a376e821c6204aaf2334137f58c1f4fd67b8
andygout <andygout@hotmail.co.uk>
2016-05-01 12:16:17 +0100

    Refactor: Productions controller tests

--- spec/factories/production.rb
+++ spec/factories/production.rb
@@ -1,24 +1,17 @@
 FactoryGirl.define do
 
   factory :production do
-    title 'Hamlet'
-    first_date '05/08/2015'
-    last_date  '31/10/2015'
+    title       'Hamlet'
+    first_date  '05/08/2015'
+    last_date   '31/10/2015'
     association :creator, factory: :second_user
-    updater { creator }
+    updater     { creator }
 
     factory :add_production do
-      title 'Othello'
-      press_date_wording 'Press day'
-      dates_tbc_note 'Jan 2016'
-      dates_note 'Press day postponed'
-    end
-
-    factory :edit_production do
-      title 'Macbeth'
-      press_date_wording 'Press day'
-      dates_tbc_note 'Jan 2016'
-      dates_note 'Press day postponed'
+      title               'Othello'
+      press_date_wording  'Press day'
+      dates_tbc_note      'Jan 2016'
+      dates_note          'Press day postponed'
     end
   end
 
