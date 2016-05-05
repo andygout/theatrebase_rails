93bed3f961c19d0a780a49c76bc9a7222a4e7346
andygout <andygout@hotmail.co.uk>
2016-05-05 22:16:51 +0100

    Re-order: bookend_div_tags + bookend_table_tags methods

--- app/helpers/shared/markup_helper.rb
+++ app/helpers/shared/markup_helper.rb
@@ -17,18 +17,18 @@ module Shared::MarkupHelper
     cols ? " colspan='#{cols}'" : ''
   end
 
-  def bookend_div_tags table_markup, div_class=nil
-    div_class = div_class ? " class='#{div_class}'" : ''
-    "<div#{div_class}>"\
-      "#{table_markup}"\
-    "</div>"
-  end
-
   def bookend_table_tags rows_markup, table_class=nil
     table_class = table_class ? " #{table_class}" : ''
     "<table class='table#{table_class}'>"\
       "#{rows_markup}"\
     "</table>"\
+  end
+
+  def bookend_div_tags table_markup, div_class=nil
+    div_class = div_class ? " class='#{div_class}'" : ''
+    "<div#{div_class}>"\
+      "#{table_markup}"\
+    "</div>"
   end
 
   def compile_header_markup header_values
