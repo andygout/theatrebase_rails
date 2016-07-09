2af439c7e2af0650299fecfcd43fc6df31d4de30
andygout <andygout@hotmail.co.uk>
2016-07-09 22:33:29 +0100

    Amend: User model tests to call each directly on array (rather than variable)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,16 +55,18 @@ describe User, type: :model do
     end
 
     it 'valid if correct format' do
-      valid_addresses = ['user@example.com', 'USER@foo.COM', 'A_US-ER@foo.bar.org', 'first.last@foo.jp', 'alice+bob@baz.cn']
-      valid_addresses.each do |valid_address|
+      [
+        'user@example.com', 'USER@foo.COM', 'A_US-ER@foo.bar.org', 'first.last@foo.jp', 'alice+bob@baz.cn'
+      ].each do |valid_address|
         user.email = valid_address
         expect(user.valid?).to be(true), "#{valid_address.inspect} should be valid"
       end
     end
 
     it 'invalid if incorrect format' do
-      invalid_addresses = ['user@example,com', 'user_at_foo.org', 'user.name@example.', 'foor@bar_baz.com', 'foo@bar+baz.com', 'foo@bar..com']
-      invalid_addresses.each do |invalid_address|
+      [
+        'user@example,com', 'user_at_foo.org', 'user.name@example.', 'foor@bar_baz.com', 'foo@bar+baz.com', 'foo@bar..com'
+      ].each do |invalid_address|
         user.email = invalid_address
         expect(user.valid?).to be(false), "#{invalid_address.inspect} should be invalid"
       end
