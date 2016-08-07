410379f03ede2d7ce0aa77a506bec7f2abc44780
andygout <andygout@hotmail.co.uk>
2016-08-07 22:38:26 +0100

    Add: Theatre model tests

--- spec/models/theatre_spec.rb
+++ spec/models/theatre_spec.rb
@@ -6,4 +6,31 @@ describe Theatre, type: :model do
     it { should belong_to :creator }
     it { should belong_to :updater }
   end
+
+  let(:theatre) { build :theatre }
+
+  TEXT_MAX_LENGTH ||= 255
+
+  context 'valid details' do
+    it 'should be valid' do
+      expect(theatre.valid?).to be true
+    end
+  end
+
+  context 'name validation' do
+    it 'invalid if name not present' do
+      theatre.name = ' '
+      expect(theatre.valid?).to be false
+    end
+
+    it 'invalid if name exceeds length limit' do
+      theatre.name = 'a' * (TEXT_MAX_LENGTH + 1)
+      expect(theatre.valid?).to be false
+    end
+
+    it 'valid if name present and does not exceed length limit' do
+      theatre.name = 'a' * TEXT_MAX_LENGTH
+      expect(theatre.valid?).to be true
+    end
+  end
 end
