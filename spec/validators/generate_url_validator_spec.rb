9366e1014e9dc6037311f6a7d2987569fa892032
andygout <andygout@hotmail.co.uk>
2016-08-11 21:24:49 +0100

    Add: Theatre examples in generate URL validator spec

--- spec/validators/generate_url_validator_spec.rb
+++ spec/validators/generate_url_validator_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 MAX_LENGTH ||= 255
 
 describe GenerateUrlValidator, type: :validator do
-  context 'URL validation' do
+  context 'production URL validation' do
     with_model :production do
       table { |t| t.string :title }
       model { validates :title, generate_url: true }
@@ -19,6 +19,25 @@ describe GenerateUrlValidator, type: :validator do
 
     it 'valid if URL generated from string is within accepted minimum and maximum number of characters' do
       expect(Production.new(title: 'hamlet').valid?).to be true
+    end
+  end
+
+  context 'theatre URL validation' do
+    with_model :theatre do
+      table { |t| t.string :name }
+      model { validates :name, generate_url: true }
+    end
+
+    it 'invalid if URL generated from string is less than one non-whitespace character' do
+      expect(Theatre.new(name: '\':/.,…?$%#').valid?).to be false
+    end
+
+    it 'invalid if URL generated from string exceeds length limit (while string itself is within limit: ß -> ss)' do
+      expect(Theatre.new(name: "#{'a' * (MAX_LENGTH - 1)}ß").valid?).to be false
+    end
+
+    it 'valid if URL generated from string is within accepted minimum and maximum number of characters' do
+      expect(Theatre.new(name: 'hamlet').valid?).to be true
     end
   end
 end
