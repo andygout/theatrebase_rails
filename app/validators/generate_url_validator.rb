658fd96aedf8a1a639d6bcd5712d0fed524d0cf6
andygout <andygout@hotmail.co.uk>
2016-08-15 22:23:43 +0100

    Refactor: Generate URL validator

--- app/validators/generate_url_validator.rb
+++ app/validators/generate_url_validator.rb
@@ -6,26 +6,41 @@ class GenerateUrlValidator < ActiveModel::EachValidator
   MAX_LENGTH = 255
   UNIQUENESS_CLASSES = ['Theatre']
 
-  def validate_each(record, attribute, value)
-    url = generate_url(value)
-    record_class = record.class
-    readable_attr = record.class.human_attribute_name(attribute)
+  def add_error record, attribute, msg
+    record.errors.add(attribute, msg)
+  end
 
-    if url.length < MIN_LENGTH
-      msg = "#{readable_attr} creates a URL that is too short (must have at least #{MIN_LENGTH} valid character)"
-      record.errors.add(attribute, msg)
-    end
+  def readable_attr record, attribute
+    record.class.human_attribute_name(attribute)
+  end
 
-    if url.length > MAX_LENGTH
-      msg = "#{readable_attr} creates a URL that is too long (must be #{MAX_LENGTH} characters or less)"
-      record.errors.add(attribute, msg)
-    end
+  def uniqueness_msg record, attribute
+    "#{readable_attr(record, attribute)} creates a URL that is already taken"
+  end
 
+  def validate_uniqueness url, record, attribute
     uniqueness_error =
       record.id &&
-      UNIQUENESS_CLASSES.include?(record_class.to_s) &&
-      record_class.where(url: url).where.not(id: record.id).limit(1).any?
+      UNIQUENESS_CLASSES.include?(record.class.to_s) &&
+      record.class.where(url: url).where.not(id: record.id).limit(1).any?
 
-    record.errors.add(attribute, "#{readable_attr} creates a URL that is already taken") if uniqueness_error
+    add_error(record, attribute, uniqueness_msg(record, attribute)) if uniqueness_error
+  end
+
+  def length_msg length, record, attribute
+    "#{readable_attr(record, attribute)} creates a URL that is too " + (length < MIN_LENGTH ?
+      "short (must have at least #{MIN_LENGTH} valid character)" :
+      "long (must be #{MAX_LENGTH} characters or less)")
+  end
+
+  def validate_length length, record, attribute
+    length_error = length < MIN_LENGTH || length > MAX_LENGTH
+    add_error(record, attribute, length_msg(length, record, attribute)) if length_error
+  end
+
+  def validate_each(record, attribute, value)
+    url = generate_url(value)
+    validate_length(url.length, record, attribute)
+    validate_uniqueness(url, record, attribute)
   end
 end
