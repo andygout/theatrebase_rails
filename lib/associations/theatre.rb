f1fb6adb81d5fb7414e21d66a9d141bba8c2d064
andygout <andygout@hotmail.co.uk>
2016-08-16 22:03:31 +0100

    Tighten: Presentation of associations files

--- lib/associations/theatre.rb
+++ lib/associations/theatre.rb
@@ -5,13 +5,9 @@ module Associations::Theatre
   included do
     has_many :productions, dependent: :restrict_with_error
 
-    belongs_to :creator,
-      class_name: :User,
-      foreign_key: :creator_id
+    belongs_to :creator, class_name: :User, foreign_key: :creator_id
 
-    belongs_to :updater,
-      class_name: :User,
-      foreign_key: :updater_id
+    belongs_to :updater, class_name: :User, foreign_key: :updater_id
   end
 
 end
