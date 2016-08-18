3ff40ae1ba4d3032f3fe37290aadd52c304013dc
andygout <andygout@hotmail.co.uk>
2016-08-19 00:06:51 +0100

    Make: StatusHelper module get_status_edit_components method private

--- app/helpers/shared/status_helper.rb
+++ app/helpers/shared/status_helper.rb
@@ -4,15 +4,15 @@ module Shared::StatusHelper
   include Shared::ViewsComponentsHelper
   include Users::ViewsComponentsHelper
 
-  def get_status_edit_components status_type
-    @page_title = "Edit #{status_type.to_s} status: #{get_user_page_title(@user)}"
-    @browser_tab = "#{@page_title} (user)"
-    @content_header = 'USER'
-    get_status_info
-    get_status_assigned_info(@user.send(status_type) || nil)
-  end
+  private
 
-    private
+    def get_status_edit_components status_type
+      @page_title = "Edit #{status_type.to_s} status: #{get_user_page_title(@user)}"
+      @browser_tab = "#{@page_title} (user)"
+      @content_header = 'USER'
+      get_status_info
+      get_status_assigned_info(@user.send(status_type) || nil)
+    end
 
     def user_status_params status_type
       params
