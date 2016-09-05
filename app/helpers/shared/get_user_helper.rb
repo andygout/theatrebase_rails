2e53dbf1001abb556b0880c887c4a608d505ff11
andygout <andygout@hotmail.co.uk>
2016-09-05 20:12:09 +0100

    Remove: Private from shared helper methods

--- app/helpers/shared/get_user_helper.rb
+++ app/helpers/shared/get_user_helper.rb
@@ -1,9 +1,7 @@
 module Shared::GetUserHelper
 
-  private
-
-    def get_user id
-      @user = ['new', 'create'].include?(params[:action]) ? User.new : User.find(id)
-    end
+  def get_user id
+    @user = ['new', 'create'].include?(params[:action]) ? User.new : User.find(id)
+  end
 
 end
--- app/helpers/shared/status_helper.rb
+++ app/helpers/shared/status_helper.rb
@@ -6,21 +6,19 @@ module Shared::StatusHelper
 
   MODEL = 'User'
 
-  private
+  def get_status_edit_components status_type
+    @page_title = "Edit #{status_type.to_s} status: #{get_user_page_title}"
+    @browser_tab = "#{@page_title} (#{MODEL.downcase})"
+    @content_header = MODEL.upcase
+    get_status_info
+    get_status_assigned_info(@user.send(status_type) || nil)
+  end
 
-    def get_status_edit_components status_type
-      @page_title = "Edit #{status_type.to_s} status: #{get_user_page_title}"
-      @browser_tab = "#{@page_title} (#{MODEL.downcase})"
-      @content_header = MODEL.upcase
-      get_status_info
-      get_status_assigned_info(@user.send(status_type) || nil)
-    end
-
-    def user_status_params status_type
-      params
-        .require(:user)
-        .permit("#{status_type}_attributes": [:_destroy, :id])
-        .deep_merge("#{status_type}_attributes": { assignor_id: current_user.id })
-    end
+  def user_status_params status_type
+    params
+      .require(:user)
+      .permit("#{status_type}_attributes": [:_destroy, :id])
+      .deep_merge("#{status_type}_attributes": { assignor_id: current_user.id })
+  end
 
 end
