161174cf5422161da02ab8d45aba25c6e8f2d8c0
andygout <andygout@hotmail.co.uk>
2016-09-07 23:22:18 +0100

    Add: Account activations controller test to ensure update not possible unless logged in as user of account

--- spec/controllers/account_activations/update_spec.rb
+++ spec/controllers/account_activations/update_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe AccountActivationsController, type: :controller do
-  let(:user) { create :unactivated_user }
+  let(:user) { create :user_with_creator }
   let(:user_creator) { user.creator }
   let(:user_attrs) { attributes_for :user }
 
@@ -18,9 +18,21 @@ describe AccountActivationsController, type: :controller do
       expect(user.updated_users).to include user
     end
 
-    it 'with invalid params will retain existing creator association and update updater associations' do
+    it 'with invalid params will retain existing creator and updater associations' do
       session[:user_id] = user.id
       patch :update, id: user, email: user.email, user: { password: '', password_confirmation: '' }
+      user.reload
+      expect(user.creator).to eq user_creator
+      expect(user.updater).to eq user_creator
+      expect(user_creator.created_users).to include user
+      expect(user_creator.updated_users).to include user
+      expect(user.created_users).not_to include user
+      expect(user.updated_users).not_to include user
+    end
+
+    it 'as user other than account owner' do
+      session[:user_id] = user_creator.id
+      patch :update, id: user, email: user.email, user: { password: user_attrs[:password], password_confirmation: user_attrs[:password] }
       user.reload
       expect(user.creator).to eq user_creator
       expect(user.updater).to eq user_creator
