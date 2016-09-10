1f236ebccfc5e88e73ff053588617c997131f209
andygout <andygout@hotmail.co.uk>
2016-09-10 14:26:46 +0100

    Add: Subtheatres to theatre seed (+ amend associated theatre in production seed)

--- db/seeds/03_theatres.rb
+++ db/seeds/03_theatres.rb
@@ -1,7 +1,21 @@
 user = User.first
 
-Theatre.create!(name:         'Barbican',
-                url:          'barbican',
-                alphabetise:  nil,
-                creator:      user,
-                updater:      user)
+theatre = Theatre.create!(name:           'National Theatre',
+                          url:            'national-theatre',
+                          alphabetise:    nil,
+                          sur_theatre:    nil,
+                          creator:        user,
+                          updater:        user)
+
+[
+  { name: 'Olivier Theatre',    url: 'national-theatre-olivier-theatre' },
+  { name: 'Lyttelton Theatre',  url: 'national-theatre-lyttelton-theatre' },
+  { name: 'Dorfman Theatre',    url: 'national-theatre-dorfman-theatre' }
+].each do |sub_theatre|
+  Theatre.create!(name:           sub_theatre[:name],
+                  url:            sub_theatre[:url],
+                  alphabetise:    nil,
+                  sur_theatre:    theatre,
+                  creator:        user,
+                  updater:        user)
+end
--- db/seeds/04_productions.rb
+++ db/seeds/04_productions.rb
@@ -1,12 +1,12 @@
 user = User.first
-theatre = Theatre.first
+theatre = Theatre.find_by_name('Olivier Theatre')
 
 Production.create!( title:        'Hamlet',
                     alphabetise:  nil,
                     url:          'hamlet',
-                    first_date:   '05/08/2015',
-                    press_date:   '25/08/2015',
-                    last_date:    '31/10/2015',
+                    first_date:   '07/09/2010',
+                    press_date:   '27/10/2010',
+                    last_date:    '26/01/2011',
                     theatre:      theatre,
                     creator:      user,
                     updater:      user)
